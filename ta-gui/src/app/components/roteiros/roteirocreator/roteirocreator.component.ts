5df11ce6952b90966d39c4f8ed11f384e261df53
unknown <mlbp@cin.ufpe.br>
2021-08-12 14:21:47 -0300

    Setup da criacao de questoes no form de criacao de roteiro

--- ta-gui/src/app/components/roteiros/roteirocreator/roteirocreator.component.ts
+++ ta-gui/src/app/components/roteiros/roteirocreator/roteirocreator.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgModule } from '@angular/core';
 import { RoteiroService } from 'src/app/services/roteiros.service';
+import { Questao } from '../../../../../../common/src/roteiros/questao';
 import { Roteiro } from '../../../../../../common/src/roteiros/roteiro';
 
 @Component({
@@ -11,19 +12,35 @@ import { Roteiro } from '../../../../../../common/src/roteiros/roteiro';
 export class RoteiroCreatorComponent
 {
   roteiro: Roteiro = new Roteiro("", "", "");
+  questoes: Questao[] = []
   conflitoNaCriacao: boolean = false;
-  
+  numberOfQuestions: number = 0;
+
   constructor(private roteiroService: RoteiroService) {}
 
   onMove(): void {
     this.conflitoNaCriacao = false;
   }
 
-  criarRoteiro(){
-    if(this.roteiro !== undefined){
+  removerQuestao(index: number) {
+    this.questoes.splice(index, 1);
+  }
+
+  adicionarQuestao(){
+    this.questoes.push(new Questao(""))
+  }
+
+  criarRoteiro(): void{
+    if(this.roteiro !== undefined && this.roteiroEValido()){
       console.log("Titulo: " + this.roteiro.titulo)
       console.log("Meta: " + this.roteiro.metaAssociada)
       console.log("Id: " + this.roteiro.id)
+
+      this.questoes.forEach( a => console.log(a.enunciado));
     }
+  }
+
+  roteiroEValido(): boolean {
+    return this.roteiro.titulo !== "" && this.roteiro.metaAssociada !== "" && this.roteiro.id !== ""; 
   }
 }
