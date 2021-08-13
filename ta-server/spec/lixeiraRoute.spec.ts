5183991d127f07bbf9b2ce6bc5ccd7efbc04fb8c
IvanNSBS <insbs@cin.ufpe.br>
2021-08-13 18:25:41 -0300

    Adding test to delete more than one roteiro from lixeira

--- ta-server/spec/lixeiraRoute.spec.ts
+++ ta-server/spec/lixeiraRoute.spec.ts
@@ -38,4 +38,35 @@ describe("O servidor na rota de lixeira", () => {
                 expect(err).toEqual(null)
              }); 
   }) 
+
+  it("Remove permanentemente mais de um roteiro", () => {
+    let roteiro1 = {"json":{"id" : "saas", "titulo": "SaaS", "metaAssociada":"saas"}};
+    let roteiro2 = {"json":{"id" : "saas2", "titulo": "SaaS2", "metaAssociada":"saas2"}};
+
+    return request.post(base_url + "roteiro", roteiro1)
+             .then(body => {
+                expect(body).toEqual({success: "O roteiro foi cadastrado com sucesso"});
+                return request.post(base_url + "roteiro", roteiro2).then(body => {
+                    expect(expect(body).toEqual({success: "O roteiro foi cadastrado com sucesso"}))
+                    
+                    return request.delete(base_url + "roteiro/saas")
+                        .then(body => {
+                            expect(body).toEqual('{"success":"O roteiro foi enviado para a lixeira"}')
+                            
+                            return request.delete(base_url + "roteiro/saas2")
+                            .then(body => {
+                                expect(body).toEqual('{"success":"O roteiro foi enviado para a lixeira"}')
+                              
+                                return request.delete(base_url + "roteiro/lixeira/saas,saas2")
+                                    .then(body => {
+                                        expect(body).toEqual('{"success":"Os roteiros foram deletados permanentemente"}')
+                                    })
+                            })
+                        })
+                })
+             })
+             .catch(err => {
+                expect(err).toEqual(null)
+             }); 
+  }) 
 })
