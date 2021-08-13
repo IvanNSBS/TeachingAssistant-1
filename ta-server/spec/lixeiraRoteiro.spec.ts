720f5a3e99386d77b50ddafa9edb4e2b5478f2cb
IvanNSBS <insbs@cin.ufpe.br>
2021-08-13 18:49:34 -0300

    Adding test to restore more than one roteiro

--- ta-server/spec/lixeiraRoteiro.spec.ts
+++ ta-server/spec/lixeiraRoteiro.spec.ts
@@ -46,4 +46,16 @@ describe("A lixeira de roteiros", () => {
     expectRoteiroToHaveThisData(roteiro[0], "saas_0", "Software As A Service", "Software As A Service")
     expect(roteiro[0].questoes.length).toBe(0);
   })
+
+  it("restaura mais de um item da lixeira corretamente", () => {
+    cadastrarRoteiro("saas_0", "Software As A Service", "Software As A Service");
+    cadastrarRoteiro("saas_1", "Software As A Service1", "Software As A Service1");
+
+    let roteiro = lixeira.restaurarRoteiros(["saas_0", "saas_1"]);
+    expect(roteiro.length).toBe(2);
+    expectRoteiroToHaveThisData(roteiro[0], "saas_0", "Software As A Service", "Software As A Service")
+    expectRoteiroToHaveThisData(roteiro[1], "saas_1", "Software As A Service1", "Software As A Service1")
+    expect(roteiro[0].questoes.length).toBe(0);
+    expect(roteiro[1].questoes.length).toBe(0);
+  })
 })
