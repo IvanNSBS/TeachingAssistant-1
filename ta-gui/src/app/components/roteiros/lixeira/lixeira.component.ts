882e2f02476cb215666050eda8f11057ef3e62d9
IvanNSBS <insbs@cin.ufpe.br>
2021-08-13 16:19:43 -0300

    Can select which roteiros will be restored or deleted

--- ta-gui/src/app/components/roteiros/lixeira/lixeira.component.ts
+++ ta-gui/src/app/components/roteiros/lixeira/lixeira.component.ts
@@ -5,18 +5,30 @@ import { Roteiro } from '../../../../../../common/src/roteiros/roteiro';
 @Component({
    selector: 'app-root',
    templateUrl: './lixeira.component.html',
-   styleUrls: ['./lixeira.component.css']
+   styleUrls: ['../roteiroCreator/roteiroCreator.component.css', '../roteiros/roteiros.component.css']
  })
 export class LixeiraComponent implements OnInit
 {
   roteiros: Roteiro[] = [];
+  selecionados: boolean[] = []
+  algoSelecionado: boolean = false;
 
   constructor(private lixeiraService: LixeiraService) {}
+
+  algoFoiSelecionado(): void{
+    this.algoSelecionado = this.selecionados.filter(a => a === true).length > 0;
+
+    this.selecionados.forEach(a => "selecionado: " + a)
+    console.log("slecionado..." + this.algoSelecionado)
+  }
 
   ngOnInit(): void {
     this.lixeiraService.getRoteiros()
           .subscribe(
-            as => { this.roteiros = as; },
+            as => { 
+              this.roteiros = as; 
+              this.roteiros.forEach(a => this.selecionados.push(false))
+            },
             msg => { alert(msg.message); }
           );
   }
