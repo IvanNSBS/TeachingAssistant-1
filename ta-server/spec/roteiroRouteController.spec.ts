5ac1bdde9b006cfa0758b49f295329f42620a236
unknown <mlbp@cin.ufpe.br>
2021-08-12 22:12:57 -0300

    Iniciando testes de servidor dos roteiros

---
+++ ta-server/spec/roteiroRouteController.spec.ts
@@ -0,0 +1,22 @@
+import request = require("request-promise");
+import { closeServer } from '../ta-server';
+
+var base_url = "http://localhost:3000/";
+
+describe("O servidor na rota de roteiros", () => {
+  var server:any;
+
+  beforeAll(() => {server = require('../ta-server')});
+
+  afterAll(() => {server.closeServer()});
+
+  it("inicialmente retorna uma lista de roteiros vazia", () => {
+    return request.get(base_url + "roteiro")
+            .then(body => 
+               expect(body).toBe("[]")
+             )
+            .catch(e => 
+               expect(e).toEqual(null)
+             );
+  })
+})
