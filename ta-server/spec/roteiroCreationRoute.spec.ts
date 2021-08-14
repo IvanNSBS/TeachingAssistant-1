eac9a60e9ee5b97eea1ed784765165409ec01e29
unknown <mlbp@cin.ufpe.br>
2021-08-14 19:48:37 -0300

    Adicionando teste de servidor de nao aceitar roteiro duplicado

--- ta-server/spec/roteiroCreationRoute.spec.ts
+++ ta-server/spec/roteiroCreationRoute.spec.ts
@@ -35,4 +35,31 @@ describe("O servidor na rota de roteiros", () => {
                 expect(err).toEqual(null);
              }); 
   })
+
+  it("nao aceita roteiros duplicados", () => {
+    let roteiro = {"json":{"id" : "ger_proj", "titulo": "Gerencia de Projetos", "metaAssociada":"ger_proj"}};
+    let roteiro2 = {"json":{"id" : "ger_proj", "titulo": "Gerencia de Projetos 2", "metaAssociada":"ger_proj2"}};
+    
+    let resposta = '{"id":"ger_proj","titulo":"Gerencia de Projetos","metaAssociada":"ger_proj"}';
+    let resposta2 = '{"id":"ger_proj","titulo":"Gerencia de Projetos 2","metaAssociada":"ger_proj2"}';
+
+    return request.post(base_url + "roteiro", roteiro)
+             .then(body => {
+                expect(body).toEqual({success: "O roteiro foi cadastrado com sucesso"});
+
+                return request.post(base_url + "roteiro", roteiro2)
+                  .then(body => {
+                    expect(body).toEqual({failure: 'O roteiro não pode ser cadastrado'})
+
+                    return request.get(base_url + "roteiro")
+                    .then(body => {
+                      expect(body).toContain(resposta);
+                      expect(body).not.toContain(resposta2)
+                    })
+                  })
+             })
+             .catch(err => {
+                expect(err).toEqual(null);
+             }); 
+  })
 })
