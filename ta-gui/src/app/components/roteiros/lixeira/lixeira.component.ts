8ce6873291812854fb335f6febc83a9d2940a6c6
IvanNSBS <insbs@cin.ufpe.br>
2021-08-18 14:28:08 -0300

    Fixing selecionados not updated after perma-del

--- ta-gui/src/app/components/roteiros/lixeira/lixeira.component.ts
+++ ta-gui/src/app/components/roteiros/lixeira/lixeira.component.ts
@@ -46,7 +46,11 @@ export class LixeiraComponent implements OnInit
     });
     
     this.lixeiraService.deletarPermanentemente(roteiroIds).subscribe(
-      as => { this.roteiros = this.roteiros.filter(roteiro => !this.containsKey(roteiroIds, roteiro.id)); },
+      as => { 
+        this.roteiros = this.roteiros.filter(roteiro => !this.containsKey(roteiroIds, roteiro.id)); 
+        this.selecionados = []
+        this.roteiros.forEach(a => this.selecionados.push(false));
+      },
       msg => { alert(msg.message); }
     );
   }
